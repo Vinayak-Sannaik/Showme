233f82a4b86d46e19b6a31f1cd6659450c98eb27
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2024-10-14 13:32:51 +0530

    responsive

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,10 +5,10 @@ const About = () => {
   return (
     <div className="about-container">
       <img src={img} alt="About" />
-
-      <h2>Vinayak Sannaik</h2>
-      <h3>Frontend developer @ TCS</h3>
-      <h2>Expert in Engineering || Best in development</h2>
+      
+      <h2 className='heading'>Vinayak Sannaik</h2>
+      <h3 className='sub-heading'>Frontend developer @ TCS</h3>
+      <h2 className='heading'>Expert in Engineering || Best in development</h2>
 
     </div>
   );
